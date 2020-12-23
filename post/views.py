daa09afd1cf2c697f009aeeab2fefc45fe3662d8
Hossam <hossamfathi050@outlook.com>
2020-12-23 22:36:20 +0300

    Modified Code views

--- post/views.py
+++ post/views.py
@@ -8,13 +8,13 @@ from django.views.generic import ListView, DetailView, UpdateView
 
 class PostList(ListView):
     model = Post
+class PostUpdate(UpdateView):
+    model = Post
+    fields =['title','content','category']
+    template_name='post/post_edit.html'
 
 
 class PostDetail(DetailView):
     model = Post
 
 
-class PostUpdate(UpdateView):
-    model = Post
-    fields =['title','content']
-    template_name='post/post_edit.html'
