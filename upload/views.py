e9c3f07f50de3e35b8dec3a8100d22d8db4aa4ed
Hossam <hossamfathi050@outlook.com>
2021-02-11 15:07:11 +0300

    upload templates

--- upload/views.py
+++ upload/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import FileAdmin
 
 # Create your views here.
 def home(request):
@@ -8,4 +10,11 @@ def home(request):
 
 
 def download(request,path):
-    file_path=os.path(' mypath/', csrf_exempt(GraphQLView.as_view(graphiql=True)))
+    file_path=os.path.join(settings.MEDIA_ROOT,path)
+    if os.path.exists(file_path):
+        with open(file_path,'rb')as fh:
+            response=HttpResponse(fh.read(),content_type="application/adminupload")
+            response['content-Disposition']='inline;filename='+os.path.basename(file_path)
+            return response
+
+        raise Http404
