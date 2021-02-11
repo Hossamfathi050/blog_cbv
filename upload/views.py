82276c0ea50005865ce1b775e1393621da058d0d
Hossam <hossamfathi050@outlook.com>
2021-02-11 14:53:44 +0300

    upload views

--- upload/views.py
+++ upload/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    context={'file':FileAdmin.objects.all()}
+    return render(request,'upload/home.html',context)
