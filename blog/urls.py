19fe0d5b6696f6400101e31c7072d56e1ec5a3ac
Hossam <hossamfathi050@outlook.com>
2021-02-11 14:27:44 +0300

    modified settings settings.DEBUG

--- blog/urls.py
+++ blog/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.templatetags.static import static
+from blog import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -22,3 +24,7 @@ urlpatterns = [
 
 ]
 
+if settings.DEBUG:
+	urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
