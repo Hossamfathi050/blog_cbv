fefa628b6e99cde225d09d7ce97d4112053a4ce0
Hossam <hossamfathi050@outlook.com>
2021-02-11 14:59:56 +0300

    upload templates

--- upload/views.py
+++ upload/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def home(request):
     context={'file':FileAdmin.objects.all()}
     return render(request,'upload/home.html',context)
+
+
+
+def download(request,path):
+    file_path=os.path(' mypath/', csrf_exempt(GraphQLView.as_view(graphiql=True)))
